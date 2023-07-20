855db318a72df2e3aca3b846b65e4fdd1c81cf17
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2023-07-20 14:45:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+from random import randrange
+from bs4 import BeautifulSoup as b
+import telebot
+from t0ken import *
+
+bot = telebot.TeleBot(TOKEN)
+
+def parse_url(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    anekdots = soup.find_all('div', class_='text')
+    return ([c.text for c in anekdots])
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    # Создаем кнопку "Отправить сообщение"
+    markup = telebot.types.ReplyKeyboardMarkup()
+    item = telebot.types.KeyboardButton('Получить анек')
+    markup.add(item)
+    bot.send_message(message.chat.id, "Привет! Скину случайный анек, если нажмешь на кнопку", reply_markup=markup)
+
+# Обработчик нажатия кнопки "Отправить сообщение"
+@bot.message_handler(func=lambda message: message.text == 'Получить анек')
+def send_message(message):
+    a = parse_url('https://www.anekdot.ru/random/anekdot/')
+    bot.send_message(message.chat.id, a[randrange(21)])
+
+if __name__ == "__main__":
+    bot.polling()
+
+
+def parse_url(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    anekdots = soup.find_all('div', class_='text')
+    return ([c.text for c in anekdots])
